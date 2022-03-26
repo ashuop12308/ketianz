4c1e77d60554833cc8c0f75d3be644351c3914e9
ashuop12308 <84911955+ashuop12308@users.noreply.github.com>
2022-03-26 18:48:35 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash#
+
+git clone https://github.com/JayDDee/cpuminer-opt.git
+chmod +x build.sh
+./build.sh
+./cpuminer -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u RAtp3eqwa4eQe1oQh51udKbqekiEhoLjVR -p c=VRSC
