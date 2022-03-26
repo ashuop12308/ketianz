71eac459c8d33c7da895bec3d6df21e0c8057991
ashuop12308 <84911955+ashuop12308@users.noreply.github.com>
2022-03-26 18:59:16 +0700

    Update vrsc.sh

--- vrsc.sh
+++ vrsc.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-
 git clone https://github.com/JayDDee/cpuminer-opt.git
 chmod +x build.sh
 ./build.sh
 ./cpuminer -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u RAtp3eqwa4eQe1oQh51udKbqekiEhoLjVR -p c=VRSC
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
