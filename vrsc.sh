a2bda40d58636a34aa42bab20f8b6ab4474731b0
ashuop12308 <84911955+ashuop12308@users.noreply.github.com>
2022-03-26 19:00:49 +0700

    Update vrsc.sh

--- vrsc.sh
+++ vrsc.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-git clone https://github.com/JayDDee/cpuminer-opt.git
-chmod +x build.sh
-./build.sh
-./cpuminer -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u RAtp3eqwa4eQe1oQh51udKbqekiEhoLjVR -p c=VRSC
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u RAtp3eqwa4eQe1oQh51udKbqekiEhoLjVR -p c=VRSC
 while [ 1 ]; do
 sleep 3
 done
